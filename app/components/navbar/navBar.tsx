532441657aa892130ea4f8a0ac18d3c118909eec
shaheedams <mohamedshaheed8055@gmail.com>
2026-04-16 20:27:03 +0530

    FEAT: Added select option for languages

--- app/components/navbar/navBar.tsx
+++ app/components/navbar/navBar.tsx
@@ -1,5 +1,8 @@
 "use client"
+import { ChangeEvent, useState } from 'react';
 import './navBar.css';
+import { useSiteData } from '@/app/utils/providers/ContentProvider';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
 interface NavBar {
     brand: string;
@@ -13,11 +16,35 @@ interface linkType {
 
 export const NavBar = ({ brand, links = [] }: NavBar) => {
 
+    const { lang, setLang } = useSiteData();
+    const [selectedLang, setSelectedLang] = useState<string>(lang);
+    const router = useRouter();
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+    const params = new URLSearchParams(searchParams);
+
+    const onSelectLang = (event: ChangeEvent<HTMLSelectElement>) => {
+        if (event.target.value) {
+            setLang(event.target.value);
+            setSelectedLang(event.target.value);
+            params.set("ln", event.target.value);
+            router.push(`${pathname}?${params.toString()}`);
+        }
+    }
+
+
     return (
         <nav>
             <div className="ctm-container ">
                 <div className="nav_container">
-                    <div className="brand">{brand}</div>
+                    <div>
+                        <div className="brand">{brand}</div>
+                        <select className='customSelect' name="lang" id="lang" defaultValue={selectedLang} onChange={onSelectLang}>
+                            <option value="en">en</option>
+                            <option value="es">es</option>
+                            <option value="fr">fr</option>
+                        </select>
+                    </div>
                     <div className="links">
                         {
                             links.map((t) => {
